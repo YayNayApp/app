6ead1342c7ccf27a5fd1aa3b2c604fe30f458e07
Asbjørn Enge <asbjorn@hanafjedle.net>
2021-06-09 09:08:22 +0200

    Fxixed footer links

--- src/shared/components/footer.js
+++ src/shared/components/footer.js
@@ -4,10 +4,10 @@ import './footer.css'
 export default function Footer(props) {
   return (
     <div className="Footer">
-      <a className="link" target="_blank" rel="noreferrer" href="https://asbjornenge.com/tezid/">About</a>
-      <a className="icon firstIcon" target="_blank" rel="noreferrer" href="https://github.com/tezid"><AiFillGithub /></a>
-      <a className="icon lastIcon" target="_blank" rel="noreferrer" href="https://twitter.com/tezid_net"><AiFillTwitterCircle /></a>
-      <a className="link" target="_blank" rel="noreferrer" href="https://github.com/tezid/docs/blob/main/LEGAL.md">Legal</a>
+      <a className="link" target="_blank" rel="noreferrer" href="https://blog.yaynay.app/">About</a>
+      <a className="icon firstIcon" target="_blank" rel="noreferrer" href="https://github.com/yaynayapp"><AiFillGithub /></a>
+      <a className="icon lastIcon" target="_blank" rel="noreferrer" href="https://twitter.com/appyaynay"><AiFillTwitterCircle /></a>
+      <a className="link" target="_blank" rel="noreferrer" href="https://github.com/yaynayapp/docs/blob/main/LEGAL.md">Legal</a>
     </div>
   )
 }
